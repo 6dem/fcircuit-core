880bb470ffe3b58585a4a987c07c6c8c3ad23604
Edem <edem.ablaev57@yandex.ru>
2025-08-19 16:06:12 +0300

    build: fix named exports to work in CJS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-export { default as AIG } from "./core/aig.js"
-export { default as Circuit } from "./core/circuit.js"
-export { default as IG } from "./core/ig.js"
-export { default as MIG } from "./core/mig.js"
+import AIG from "./core/aig.js"
+import Circuit from "./core/circuit.js"
+import IG from "./core/ig.js"
+import MIG from "./core/mig.js"
+
+export { AIG, Circuit, IG, MIG }
 
 export * from "./core/aig-nodes.js"
 export * from "./core/ig-nodes.js"
